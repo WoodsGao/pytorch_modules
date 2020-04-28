d5a192cf842d0d3fa54807cca5d7b56cf0ef44a2
woodsgao <woodsgao@outlook.com>
2020-04-28 18:35:40 +0800

    a better fpn

--- pytorch_modules/nn/fpn.py
+++ pytorch_modules/nn/fpn.py
@@ -10,45 +10,34 @@ class FPN(nn.Module):
         """[summary]
         
         Arguments:
-            channels_list {list} -- channels of feature maps, from low levels to high levels
+            channels_list {list} -- channels of feature maps, from high levels to low levels
         
         Keyword Arguments:
             out_channels {int} -- out channels  (default: {128})
             reps {int} -- repeat times (default: {3})
         """
         super(FPN, self).__init__()
-        assert reps > 0
-        first_conv = []
-        for idx, channels in enumerate(channels_list):
-            first_conv.append(ConvNormAct(channels, out_channels))
-        self.first_conv = nn.ModuleList(first_conv)
-
-        fpn_list = []
-        for i in range(reps):
-            fpn_stage = []
-            for idx, channels in enumerate(channels_list):
-                if idx != 0:
-                    in_channels = 2 * out_channels
-                else:
-                    in_channels = out_channels
-                fpn_stage.append(ConvNormAct(in_channels, out_channels))
-            fpn_stage = nn.ModuleList(fpn_stage)
-            fpn_list.append(fpn_stage)
-        self.fpn_list = nn.ModuleList(fpn_list)
+        self.fpn_list = nn.ModuleList([])
+        last_channels = 0
+        for i in range(len(channels_list)):
+            in_channels = channels_list[i] + last_channels
+            fpn = [ConvNormAct(in_channels, out_channels)]
+            fpn += [ConvNormAct(out_channels, out_channels)] * reps
+            fpn = nn.Sequential(*fpn)
+            self.fpn_list.append(fpn)
+            last_channels = out_channels
 
     def forward(self, features):
-        features = [conv(f) for conv, f in zip(self.first_conv, features)]
-        for fpn in self.fpn_list:
-            new_features = []
-            for idx, (feature, stage) in enumerate(zip(features[::-1], fpn)):
-                if idx > 0:
-                    last_feature = new_features[-1]
-                    last_feature = F.interpolate(last_feature,
-                                                 scale_factor=2,
-                                                 mode='bilinear',
-                                                 align_corners=False)
-                    feature = torch.cat([feature, last_feature], 1)
-                feature = stage(feature)
-                new_features.append(feature)
-            features = new_features[::-1]
-        return features
+        new_features = []
+        for i in range(len(self.fpn_list)):
+            feature = features[i]
+            if len(new_features):
+                last_feature = new_features[-1]
+                last_feature = F.interpolate(last_feature,
+                                             scale_factor=2,
+                                             mode='bilinear',
+                                             align_corners=False)
+                feature = torch.cat([last_feature, feature], 1)
+            feature = self.fpn_list[i](feature)
+            new_features.append(feature)
+        return new_features
